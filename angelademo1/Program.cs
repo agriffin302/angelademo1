4d4d9f49e287ce5ba6847f0f004c4fa41464bb8c
Angela Griffin <Angela.Griffin@bcbsnc.com>
2016-10-31 17:03:09 -0400

    demo1
    First Lab - demo

---
+++ angelademo1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace angelademo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myStuff = new string[3];
+            myStuff[0]= "Angela";
+            myStuff[1]= "October";
+            myStuff[2]= "Black";
+
+            for (int i = 0; i < 3; i = i + 1)
+            {
+                Console.WriteLine(myStuff[i]);
+                       
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to close the window...");
+            Console.ReadLine();
+        }
+    }
+}
